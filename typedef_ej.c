a8d17bfec2f31c9ca0e5938a9e686e4d52f9c3db
Victor Benito <victorbenito8@gmail.com>
2024-02-22 12:21:36 +0100

    typedef

---
+++ typedef_ej.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef long long int int64;
+typedef int int32;
+typedef short int16;
+typedef char byte;
+
+typedef struct{
+  int64 numeroHojas;
+  int32 numeroRamas;
+  int16 numeroDulces;
+  byte numeroArbol[10];
+}arbol_t;
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
