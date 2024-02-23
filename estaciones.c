cede1c1926eb6e688288550975a8ba11501f6c1c
Victor Benito <victorbenito8@gmail.com>
2024-02-23 12:17:32 +0100

    estaciones del año

---
+++ estaciones.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum estaciones {
+  primavera, verano, otonno, invierno
+};
+int main(int argc, char *argv[]) {
+  printf("%d%d%d%d\n", primavera,verano,otonno,invierno);
+  return 0;
+}
